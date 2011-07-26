d8823c041ac96af6ad7b5fde1d7d3ba124d9a6c3
APR <apr@apr-XPS-L701X.(none)>
2011-07-26 16:12:43 -0500

    Modifying uninstall hook to remove GrailsGeronimoConfig.groovy

--- scripts/_Uninstall.groovy
+++ scripts/_Uninstall.groovy
@@ -3,3 +3,7 @@
 // Use this script if you intend to do any additional clean-up on uninstall, but
 // beware of messing up SVN directories!
 //
+
+// Remove configuration file
+ant.delete(file:"${basedir}/grails-app/conf/grails-geronimo/_GeronimoConfig.groovy", failonerror:false)
+
